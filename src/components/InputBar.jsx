aecc52be86f69a3518be2c6eae4e6987b23a47f7
Sarthak30-03-2003 <sarthak8544@gmail.com>
2022-03-17 00:14:20 +0530

    Final commit - Sarthak

--- src/components/InputBar.jsx
+++ src/components/InputBar.jsx
@@ -8,9 +8,13 @@ const InputBar = ({handelClick,handelChange}) => {
     return (
         <div className="container">
                 <div className="form-group d-flex justify-content-center align-items-center input-width">
-                    <input onChange={(e)=>handelChange(e.target.value)} className="form-control" ref={data} placeholder="🔎 INPUT WALLET ADDRESS"/>
-                    <button onClick={()=>handelClick(data.current.value)} className="btn btn-primary m-5">SHOW All NFT`S</button>
+                    {/* <input onChange={(e)=>handelChange(e.target.value)} className="form-control search-bar-main" ref={data} placeholder="🔎 INPUT WALLET ADDRESS"/> */}
+                    {/* <button onClick={()=>handelClick(data.current.value)} className="btn btn-primary m-5 search-bar-icon"></button> */}
                 </div>
+                <div class="search-bar">
+        <input type="text" ref={data} onChange={(e)=>handelChange(e.target.value)} placeholder="Search your address &#128270"  class=" search-bar-main"></input>
+        <div ><button class="search-bar-icon btn btn-primary" title="Search" onClick={()=>handelClick(data.current.value)}></button ></div>
+    </div>
         </div>
     );
 }
